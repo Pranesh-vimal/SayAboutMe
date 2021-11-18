727a253f3423185aa78515c3453a82a5609a43df
Pranesh P <Pranesh.vimal5@gmail.com>
2021-11-18 13:08:47 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php 
 
-$servername ="sql106.epizy.com";
-$dbUser = "epiz_23933252";
-$dbPass = "kavithakps1";
+$servername ="";
+$dbUser = "";
+$dbPass = "";
 $dbName = "epiz_23933252_sam";
 
 $con = mysqli_connect("$servername","$dbUser","$dbPass","$dbName");
